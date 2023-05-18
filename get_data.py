11e852477e7e4690089c6de2cde49d5d81a504e1
joshc3453 <joshc3453@gmail.com>
2023-05-18 13:53:33 -0400

    Adding initial files

---
+++ get_data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sodapy import Socrata
+import config_file
+
+data_url = 'healthdata.gov'
+data_set = 'j8mb-icvb'
+app_token = config_file.app_token
+
+client = Socrata(data_url, config_file.app_token) 
+
+client.timeout = 60
+
+results = client.get(data_set, limit=200000)
+
+df = pd.DataFrame.from_records(results)
